0934746536b2dce6e1290ab9ce0661be41059a28
Xatai Azimov <ezimovxetai011@gmail.com>
2026-04-05 03:23:09 +0400

    Update admin routes for product and employee deletion

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,15 @@ use Illuminate\Support\Facades\Route;
 // Ana səhifə
 Route::get('/', [ProductController::class, 'index']);
 
-// Admin Paneli (Giriş)
+// Admin Paneli
 Route::get('/admin', [ProductController::class, 'admin'])->name('admin');
 
 // Məhsul Əməliyyatları
 Route::post('/admin/add', [ProductController::class, 'store'])->name('product.store');
-Route::delete('/admin/delete/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
+// Bura GET etdik:
+Route::get('/admin/delete/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
 
 // İşçi Əməliyyatları
 Route::post('/admin/employee', [ProductController::class, 'storeEmployee'])->name('employee.store');
-Route::delete('/admin/employee/{id}', [ProductController::class, 'destroyEmployee'])->name('employee.destroy');
+// Bura da GET etdik:
+Route::get('/admin/employee/delete/{id}', [ProductController::class, 'destroyEmployee'])->name('employee.destroy');
